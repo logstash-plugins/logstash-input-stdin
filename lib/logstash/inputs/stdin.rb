484e3b6ee938b4bf7cd254289c5195bf2998fdca
Colin Surprenant <colin.surprenant@gmail.com>
2015-05-11 12:01:30 -0400

    fix shutdown exception and bump version to 0.1.6

--- lib/logstash/inputs/stdin.rb
+++ lib/logstash/inputs/stdin.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 require "logstash/inputs/base"
 require "logstash/namespace"
+require "concurrent/atomics"
 require "socket" # for Socket.gethostname
 
 # Read events from standard input.
@@ -12,14 +13,18 @@ class LogStash::Inputs::Stdin < LogStash::Inputs::Base
 
   default :codec, "line"
 
-  public
+  def initialize(*args)
+    super(*args)
+    @stop_requested = Concurrent::AtomicBoolean.new(false)
+  end
+
   def register
     @host = Socket.gethostname
     fix_streaming_codecs
-  end # def register
+  end
 
-  def run(queue) 
-    while true
+  def run(queue)
+    while @stop_requested.false?
       begin
         # Based on some testing, there is no way to interrupt an IO.sysread nor
         # IO.select call in JRuby. Bummer :(
@@ -31,16 +36,21 @@ class LogStash::Inputs::Stdin < LogStash::Inputs::Base
         end
       rescue IOError, EOFError, LogStash::ShutdownSignal
         # stdin closed or a requested shutdown
+        @stop_requested.make_true
         break
+      rescue => e
+        # ignore any exception in the shutdown process
+        break if @stop_requested.true?
+        raise(e)
       end
-    end # while true
+    end
     finished
-  end # def run
+  end
 
-  public
   def teardown
+    @stop_requested.make_true
     @logger.debug("stdin shutting down.")
     $stdin.close rescue nil
     finished
-  end # def teardown
-end # class LogStash::Inputs::Stdin
+  end
+end
--- logstash-input-stdin.gemspec
+++ logstash-input-stdin.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
 
   s.name            = 'logstash-input-stdin'
-  s.version         = '0.1.5'
+  s.version         = '0.1.6'
   s.licenses        = ['Apache License (2.0)']
   s.summary         = "Read events from standard input"
   s.description     = "This gem is a logstash plugin required to be installed on top of the Logstash core pipeline using $LS_HOME/bin/plugin install gemname. This gem is not a stand-alone program"
@@ -26,6 +26,8 @@ Gem::Specification.new do |s|
   s.add_runtime_dependency 'logstash-codec-line'
   s.add_runtime_dependency 'logstash-codec-json'
   s.add_runtime_dependency 'logstash-codec-json_lines'
+  s.add_runtime_dependency 'concurrent-ruby'
+
   s.add_development_dependency 'logstash-devutils'
 end
 
